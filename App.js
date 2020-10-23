60b06f1e338d0741b1ad59e89b3812028f8e6ff9
legion-11 <legalionion@gmail.com>
2020-10-23 19:18:48 +0300

    add screens in stack

--- App.js
+++ App.js
@@ -13,32 +13,53 @@ import SignIn from './app/components/SignIn'
 import ViewPatients from './app/components/ViewPatients'
 import ViewPatient from './app/components/ViewPatient'
 import AddPatient from './app/components/AddPatient'
+import ViewVitals from './app/components/ViewVitals'
+import SignUp from './app/components/SignUp'
+import AddVitals from './app/components/AddVitals'
 
 export default App = () => {
   const Stack = createStackNavigator();
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="SignIn"
-          component={SignIn}
-          options={{ title: 'Welcome' }}
-        />
+        <Stack.Navigator>
+          <Stack.Screen
+            name="SignIn"
+            component={SignIn}
+            options={{ title: 'Welcome' }}
+          />
 
-        <Stack.Screen name="ViewPatients" component={ViewPatients} />
+           <Stack.Screen
+            name="SignUp"
+            component={SignUp}
+            options={{ title: 'Sign Up' }}
+          />
 
-        <Stack.Screen
-          name="ViewPatient"
-          component={ViewPatient}
-          options={({ route }) => ({ title: route.params.patient.name })}
-        />
+          <Stack.Screen name="ViewPatients" component={ViewPatients} />
 
-        <Stack.Screen
-          name="AddPatient"
-          component={AddPatient}
-          options={({ route }) => ({ title: route.params.patient.name })}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+          <Stack.Screen
+            name="ViewPatient"
+            component={ViewPatient}
+            options={({ route }) => ({ title: route.params.patient.name })}
+          />
+
+           <Stack.Screen
+            name="ViewVitals"
+            component={ViewVitals}
+            options={({ route }) => ({ title: "Vitals " + route.params.patient.name })}
+          />
+
+           <Stack.Screen
+            name="AddVitals"
+            component={AddVitals}
+            options={({ route }) => ({ title: "Vitals " + route.params.patient.name })}
+          />
+
+          <Stack.Screen
+            name="AddPatient"
+            component={AddPatient}
+            options={({ route }) => ({ title: route.params.patient.name })}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 };
